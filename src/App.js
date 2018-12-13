db4e1d78ddc8c18b56fcb7ea858d5d284eaf3513
markova <markova@reksoft.ru>
2018-12-13 13:21:14 +0300

    Replacing App from function to component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Form from './components/Form';
 import List from './components/List';
 
 import withCrud from './wrappers/withCrud';
 
-const App = ({data, create, update, remove}) => {
-    return (
-        <div>
-            <h1>App</h1>
-            <Form onSubmit={create}/>
+class App extends Component {
+    render() {
+        const {data, create, update, remove} = this.props;
 
-            <List
-                todos={data}
-                onToggle={update}
-                onRemove={remove}/>
-        </div>
-    );
+        return (
+            <div>
+                <Form onSubmit={create}/>
+
+                <List
+                    todos={data}
+                    onToggle={update}
+                    onRemove={remove}/>
+            </div>
+        );
+    }
 };
 
 export default withCrud(App, '/api/todos');
